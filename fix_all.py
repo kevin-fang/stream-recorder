8c6863ef8a3f22b111bfc98d7f0d5a98d29484e8
Kevin Fang <kfang1233@gmail.com>
2021-07-13 18:53:44 -0400

    Add logging and threading to fixer

--- fix_all.py
+++ fix_all.py
@@ -1,6 +1,10 @@
 import glob
 import subprocess
 import os
+import threading
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 QUALITY = "best"
 RECORDINGS_FOLDER = "recordings/"
@@ -14,17 +18,27 @@ def ffmpeg_copy_and_fix_errors(recorded_filename, processed_filename):
             [ffmpeg_path, "-err_detect", "ignore_err", "-i", recorded_filename, "-c", "copy",
                 processed_filename])
         os.remove(recorded_filename)
-        print("Processed file saved as {}".format(processed_filename))
+        logging.info("Processed file saved as {}".format(processed_filename))
     except Exception as e:
-        print(e)
+        logging.error(e)
 
 
 def walk():
+    jobs = []
     for filename in glob.iglob(RECORDINGS_FOLDER + '**/*.mp4', recursive=True):
-        print("Checking {}".format(filename))
+        logging.info("Checking {}".format(filename))
         if not "processed" in filename:
-            ffmpeg_copy_and_fix_errors(filename, filename.split(".mp4")[
-                                       0] + "_processed.mp4")
+            x = threading.Thread(target=ffmpeg_copy_and_fix_errors, args=(
+                filename, filename.split(".mp4")[0] + "_processed.mp4"))
+            logging.info("Fixing errors in {}".format(filename))
+            x.start()
+            jobs.append(x)
+            # ffmpeg_copy_and_fix_errors(filename, filename.split(".mp4")[0] + "_processed.mp4")
+    
+    for job in jobs:
+        job.join()
+    logging.info("Finished.")
+
 
 if __name__ == "__main__":
     walk()
